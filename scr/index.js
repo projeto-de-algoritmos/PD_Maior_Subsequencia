82411ec25febbef1aa44888abc6cf2e7e945243c
Eliás Yousef <eliasyousef00@gmail.com>
2022-09-04 16:01:55 -0300

    Projeto de maior subsequência

---
+++ scr/index.js
@@ -0,0 +1,52 @@
+const result = document.querySelector(".result")
+const form = document.querySelector(".form")
+const input = document.querySelector(".form input")
+
+vetor = []
+for (let i = 0; i < 7; i++) {
+    const randomico = parseInt(Math.random() * 10)
+    vetor.push(randomico)
+}
+// console.log(vetor)
+
+function exibeAleatorio() {
+    for (let i = 0; i < vetor.length; i++) {
+        let element = document.createElement('p')
+        element.textContent = vetor[i]
+        element.classList.add('random')
+        result.appendChild(element)
+    }
+}
+exibeAleatorio();
+
+function LIS(vetor, i, n, prev) {
+    if (i == n) {
+        return 0;
+    }
+
+    let excl = LIS(vetor, i+1, n, prev)
+
+    let incl = 0;
+    if (vetor[i] > prev) {
+        incl = 1 + LIS(vetor, i+1, n, vetor[i])
+    }
+
+    return Math.max(incl, excl)
+}
+const seq = LIS(vetor, 0, vetor.length, Number.MIN_SAFE_INTEGER);
+console.log(seq)
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const num = parseInt(input.value)
+
+    if (num == seq) {
+        window.alert("Parabéns! Você acertou o tamanho da maior subsequência!")
+    }
+    else {
+        window.alert("Opsss, essa não é a maior subsequência!")
+    }
+    input.value = "";
+})
